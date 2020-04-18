7fc475eec4d8f3fca0daaf810d783d0d3f83139f
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2020-04-18 19:51:51 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,14 +1,8 @@
+# Default node resources
+node default {
+}
+
 # Any node that starts with 'master' is a primary master for a participant
 node /^master/, /^puppet/ {
   include profile::master
-}
-
-# Any node that starts with 'cd4pe' is a participant's cd4pe node
-node /^cd4pe/ {
-  include profile::cd4pe
-}
-
-# The shared GitLab server has a short hostname of 'gitlab'
-node /^gitlab/ {
-  include profile::gitlab
 }
