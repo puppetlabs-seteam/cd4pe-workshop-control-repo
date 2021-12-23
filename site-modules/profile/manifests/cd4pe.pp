0e663892def8e73242ec003bc44c0c163e821b36
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2021-12-23 14:34:20 +0100

    Update cd4pe.pp

--- site-modules/profile/manifests/cd4pe.pp
+++ site-modules/profile/manifests/cd4pe.pp
@@ -1,20 +1,6 @@
-# This fix overrides the cd4pe repo's baseurl, so that it matches the SSL certificate name
-# Also fixes an SElinux change happening after the initial cd4pe install run
+# Set 24h run interval so that it doesn't interfere with the workshop
 
 class profile::cd4pe {
-
-  $pe_ver  = pe_build_version()
-  $base_source = 'https://puppet.classroom.puppet.com:8140/packages'
-  $source = "${base_source}/${pe_ver}/puppet_enterprise"
-
-  Yumrepo <| title == 'cd4pe' |> {
-    baseurl => $source,
-  }
-
-  File <| (title == '/etc/systemd/system/docker.service.d/service-overrides.conf') or
-          (title == '/etc/systemd/system/docker-cd4pe.service') |> {
-    seltype => 'container_unit_file_t'
-  }
 
   ini_setting { 'puppet[main:runinterval]':
     ensure            => present,
